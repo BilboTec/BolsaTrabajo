25ce18465bc24e58f787242ad56ee82a10e8a451
Josu <info@josu-astigarraga.es>
2016-12-02 10:26:47 +0100

    Añadido controlador de Migraciones

---
+++ application/controllers/Migraciones.php
@@ -0,0 +1,12 @@
+<?php
+
+class Migraciones extends CI_Controller{
+	public function index(){
+		 $this->load->library('migration');
+
+                if ($this->migration->current() === FALSE)
+                {
+                        show_error($this->migration->error_string());
+                }
+	}
+}
